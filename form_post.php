7277490e4e6b533df58e49ce35abea6e01644bf2
unknown <muhammadaliagil@gmail.com>
2022-09-19 11:27:17 -0700

    Tugas

---
+++ form_post.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form_Bio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="proses.php" method="post">
+        <table>
+            <tr>
+                <td>Nama</td>
+                <td>:</td>
+                <td>
+                    <input type="text" name="nama">
+                </td>
+            </tr>
+            <tr>
+                <td>Kelas</td>
+                <td>:</td>
+                <td>
+                    <input type="text" name="kelas">
+                </td>
+            </tr>
+        </table>
+        <tr>
+            <td colspan="3">
+                <button class="btn" type="submit">Simpan</button>
+                <button class="rst" type="reset">Reset</button>
+             </td>
+        </tr>
+    </form>
+</body>
+</html>
---
+++ get.php
@@ -0,0 +1,8 @@
+<?php
+    if(isset($_GET)){
+     $nama = $_GET['nama'];
+     $alamat = $_GET['alamat'];
+     echo "Nama Siswa : $nama <br>";
+     echo "Nama Siswa : $alamat ";
+    }
+?>
